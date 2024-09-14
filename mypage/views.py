aee5716e57f385a5555f5bd6fb5ed2c4cc882132
iizuka <iizukahayato1217@gmail.com>
2024-09-14 17:52:12 +0900

    garbage

--- mypage/views.py
+++ mypage/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
--- ramen_page/urls.py
+++ ramen_page/urls.py
@@ -21,4 +21,8 @@ from mypage import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
 ]
