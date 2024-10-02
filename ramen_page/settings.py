689a42b7f52447300125317a933f590725f0f518
iizuka <iizukahayato1217@gmail.com>
2024-10-02 23:37:17 +0900

    minor

--- ramen_page/settings.py
+++ ramen_page/settings.py
@@ -141,6 +141,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
